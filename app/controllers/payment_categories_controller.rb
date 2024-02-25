091995db26e9adde13af2d2a094bda52312aeffd
Henrique Barros <henriquebarrosx@gmail.com>
2024-02-25 12:04:14 -0300

    Create payment categories CRUD using turbo links

--- app/controllers/payment_categories_controller.rb
+++ app/controllers/payment_categories_controller.rb
@@ -1,31 +1,33 @@
 class PaymentCategoriesController < ApplicationController
-  before_action :set_payment_category, only: [:show, :edit, :update]
+  before_action :set_payment_category, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @payment_categories = PaymentCategory.all
-    end
+  def index
+    @payment_categories = PaymentCategory.all
+  end
 
-    def show
-    end
+  def show; end
 
-    def new 
-      @payment_category = PaymentCategory.new
-      turbo_stream
-    end
+  def new
+    @payment_category = PaymentCategory.new
+  end
 
-    def create
-      @payment_category = PaymentCategory.new(payment_category_params)
-      @payment_category.save
-      turbo_stream
-    end
+  def create
+    @payment_category = PaymentCategory.new(payment_category_params)
+    @payment_category.save
+    turbo_stream
+  end
 
-    def edit
-    end
+  def edit; end
 
-    def update
-      @payment_category.update(article_params)
-      turbo_stream
-    end
+  def update
+    @payment_category.update(payment_category_params)
+    turbo_stream
+  end
+
+  def destroy
+    @payment_category.destroy
+    turbo_stream
+  end
 
   private
     def payment_category_params
