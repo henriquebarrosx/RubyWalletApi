5973f2a64332cb4980bc7428e7d10b7d9a088f38
Henrique Barros <henriquebarrosx@gmail.com>
2024-02-25 01:42:30 -0300

    Setting up turbo rails

--- app/controllers/payment_categories_controller.rb
+++ app/controllers/payment_categories_controller.rb
@@ -1,27 +1,38 @@
 class PaymentCategoriesController < ApplicationController
+  before_action :set_payment_category, only: [:show, :edit, :update]
+
     def index
-      @categories = PaymentCategory.all
+      @payment_categories = PaymentCategory.all
+    end
+
+    def show
     end
 
     def new 
       @payment_category = PaymentCategory.new
+      turbo_stream
     end
 
     def create
       @payment_category = PaymentCategory.new(payment_category_params)
-      
-      if @payment_category.save
-        respond_to do |format|
-          format.js { render partial: 'payment_categories/index', locals: { categories: PaymentCategory.all } }
-          format.html { redirect_back fallback_location: payment_categories_path, notice: 'category was successfully created.' }
-        end
-      else
-          format.html { render :new }
-      end
+      @payment_category.save
+      turbo_stream
+    end
+
+    def edit
+    end
+
+    def update
+      @payment_category.update(article_params)
+      turbo_stream
     end
 
   private
     def payment_category_params
-      params.permit(:name)
+      params.require(:payment_category).permit(:name)
+    end
+
+    def set_payment_category
+      @payment_category = PaymentCategory.find(params[:id])
     end
 end
